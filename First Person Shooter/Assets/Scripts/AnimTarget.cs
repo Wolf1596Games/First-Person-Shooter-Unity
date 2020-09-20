0177f860521aad2ed79074b1fe7cec98a1c66d31
Wolf1596Games <wolf1596games@gmail.com>
2020-09-20 16:17:05 -0500

    Added animated target

---
+++ First Person Shooter/Assets/Scripts/AnimTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTarget : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    [Header("Target Stats")]
+    [SerializeField] float speed;
+    [SerializeField] int health = 15;
+
+    GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
