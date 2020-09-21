9a9abedf58e5eeb9a900efa3108fa5f44119d009
Wolf1596Games <wolf1596games@gmail.com>
2020-09-20 19:17:35 -0500

    Added hazard

---
+++ First Person Shooter/Assets/Scripts/Hazard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with" + other.gameObject.name);
+
+        if (other.CompareTag("Player"))
+        {
+            
+            int health = player.GetHealth();
+            health -= 15;
+            player.SetHealth(health);
+        }
+
+        Destroy(gameObject);
+    }
+}
